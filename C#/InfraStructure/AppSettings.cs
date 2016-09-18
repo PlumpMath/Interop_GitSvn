d933c42b870c5552d544896c4565a0bce6c4882b
Raphael Rogenski <raphaelrogenski@gmail.com>
2016-09-17 23:13:33 -0300

    Add a helper class to get configurations

---
+++ C#/InfraStructure/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+
+namespace InfraStructure
+{
+  public class AppSettings
+  {
+    protected static readonly IDictionary<string, object> Settings = new Dictionary<string, object>();
+
+    protected static T Get<T>(String propertyName, T defaultValue = default(T))
+    {
+      if (Settings.ContainsKey(propertyName))
+        return (T)Settings[propertyName];
+
+      String property = ConfigurationManager.AppSettings.Get(propertyName);
+
+      T value = defaultValue;
+
+      if (!string.IsNullOrEmpty(property))
+        value = (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromInvariantString(property);
+
+      Settings.Add(propertyName, value);
+      return value;
+    }
+  }
+}
